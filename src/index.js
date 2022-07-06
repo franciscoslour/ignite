2c51e6c4201911ebc4aca880354ec8149ba97bb9
Francisco Lourenço <franciscolour2011@gmail.com>
2022-07-06 15:47:08 +0100

    lista de cursos

--- src/index.js
+++ src/index.js
@@ -2,8 +2,41 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello, Francico" });
+app.use(express.json())
+
+let courses = ["Curso 1", "Curso 2", "Curso 3", "Curso 4"];
+
+app.get("/courses", (request, response) => {
+    let { id } = request.query;
+    if (id != undefined) {
+        let filtrados = courses.filter((curso, indice) => indice == id);
+        return response.json(filtrados);
+    }
+    return response.json(courses);
+})
+
+app.post("/courses", (request, response) => {
+    const { descricao } = request.body;
+    courses.push(descricao);
+    return response.json(courses);
+})
+
+app.put("/courses/:id", (resquest, response) => {
+    const { id } = resquest.params;
+    const { descricao } = resquest.body;
+    courses[id] = descricao;
+    return response.json(courses)
+})
+
+app.patch("/courses/:id", (request, response) => {
+    let cursos = ["Curso 1", "Curso 2", "Curso 3", "Curso 4"];
+    return response.json(cursos);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    const { id } = request.params;
+    courses.splice(id, 1);
+    return response.json(courses);
 })
 
 app.listen(3333);
